f8a563ec4062c8ef230998b4a46cbc1e0e33ed35
siddht1 <rt1yrru+github@gmail.com>
2023-01-30 12:57:20 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 // In src/index.js 
-// import fetch from 'node-fetch';
+
 const express = require("express"); 
 const app = express(); 
-var QRCode = require('qrcode')
+
 const PORT = process.env.PORT || 3002; 
 
-
-// const response = await fetch(_url);
-// const data = await response.json();
 // For testing purposes 
 app.get("/", (req, res) => { 
     res.send('{working}');
