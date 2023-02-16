1c8137f2a53211ffe1637d068a4b5434ab3b21fb
siddht1 <rt1yrru+github@gmail.com>
2023-02-16 13:45:24 +0530

    Update index.js
    CORS enabled,can be restricted using origin 
    (https://www.npmjs.com/package/cors)

--- src/v1/routes/index.js
+++ src/v1/routes/index.js
@@ -1,7 +1,10 @@
 // In src/v1/routes/index.js
 const express = require("express");
 const router = express.Router();
+const cors = require('cors');
 
+// Enable CORS for all routes
+router.use(cors());
 router.route("/").get((req, res) => {
   res.send(`<h2>Hello from ${req.baseUrl}</h2>`);
 });
