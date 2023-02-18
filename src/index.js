a03e8c9709e0dc4586912f3c186eff836ef01e45
siddht1 <rt1yrru+github@gmail.com>
2023-02-18 06:06:05 +0530

    Update index.js
    cleaned,only index.js will work

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 // In src/index.js
 const express = require("express");
-// *** ADD ***
-const v1Router = require("./v1/routes");
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -11,8 +10,6 @@ app.get("/", (req, res) => {
   res.send("<h2>It's Working!</h2>");
 });
 
-// *** ADD ***
-app.use("/api/v1", v1Router);
 
 app.listen(PORT, () => {
   console.log(`API is listening on port ${PORT}`);
